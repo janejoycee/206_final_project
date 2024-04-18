5961ce6ae9a9ee7cf629b4bd577290fdbef32a35
Jane Joyce <janejoyce@Janes-MacBook-Air-71.local>
2024-04-18 12:13:22 -0400

    debug

--- grammy.py
+++ grammy.py
@@ -57,14 +57,51 @@ def retrieve_listings():
         #print(combined_list)
         award_dic[award_name] = combined_list
 
-    print(award_dic)
-    print(len(award_dic))
+    #print(award_dic)
+   # print(len(award_dic))
     
     return award_dic
 
 
 
 def get_winners ():
+
+    relative_path = "/2023/02/grammys-2023-full-list-of-winners.html"
+
+    base_url = "https://www.vulture.com/"
+    full_url = base_url + relative_path
+
+    
+    response = requests.get(full_url)
+    if response.status_code != 200:  # Checks if the request was successful
+        print('Failed to retrieve content.')
+        return None
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    winner_dict = {}
+
+    categories =  soup.find_all('p', class_='clay-paragraph')
+    
+    for category in categories:
+        award_name_element = category.find_all('strong')[0]
+        print(award_name_element)
+        if award_name_element:
+            award_name = award_name_element.text.strip()
+            #award_name_element.extract()
+        else:
+            continue
+
+        award_winner = category.find_all('strong')[1]
+        print(award_winner)
+
+        winner_dict[award_name] = award_winner
+
+        print(winner_dict)
+    return winner_dict
+    
+
     #this function gets the winners for every category, and adds it to a list
 
         
