76e1b9a139bf3d5be086e129034f94a1c3ef4b16
Jane Joyce <janejoyce@Janes-MacBook-Air-71.local>
2024-04-18 12:05:36 -0400

    debugged and finished the first function. This has all the awards, and every nominee in that award, in a list of tuples.

--- grammy.py
+++ grammy.py
@@ -40,9 +40,8 @@ def retrieve_listings():
         #print((nominee_lines))
 
         combined_list = []
-        prev_string = ""
         # pattern = re.compile(r'“(.*?)”\s*—\s*(.*)')
-
+        prev_string = ""
         for line in nominee_lines:
             if '—' in line:
                 if prev_string:
@@ -52,8 +51,8 @@ def retrieve_listings():
             else:
                 prev_string = line  
 
-        if prev_string:  
-            combined_list.append(prev_string)
+            if prev_string:  
+                combined_list.append(prev_string)
 
         #print(combined_list)
         award_dic[award_name] = combined_list
@@ -63,81 +62,10 @@ def retrieve_listings():
     
     return award_dic
 
-    #need to a account for Album of the Year
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#     genres = soup.find_all('h3', class_='edTag')
-#     for genre in genres:
-#         genre = genre.text
-#         #print(genre)
-#         genre_list.append(genre)
-#         #should be 27
-    
-#    # print(genre_list)
-#     #print(len(genre_list))
-
-#     strong_tags_inside_p = soup.find_all('p')  # Find all <p> tags
-#     for p_tag in strong_tags_inside_p:
-#         strong_tags = p_tag.find_all('strong')  # Find all <strong> tags inside each <p> tag
-#         for strong_tag in strong_tags:
-#             strong_text = strong_tag.get_text()
-#             match = re.match(title_regex, strong_text)
-#             if match:
-#                 title = match.group(1)
-#                 #print(title)
-#                 if title not in award_dic:
-#                     award_dic[title] = []
-    
-#     print(award_dic)
-
-#     edTag_ul = soup.find_all('ul', class_='edTag')
-
-#     for edTag in edTag_ul: #for every award in a list of awards
-#         #print(edTag)
-
-#         song_and_artist_noms = edTag.find_all('li') #get every awards nominations
-
-#         winner_text = ""
-
-#         for item in song_and_artist_noms:
-#             #print(item)
-#             if "WINNER:" in item:
-#                 continue  # Skip to the next item if "WINNER:" is found
-#             text = item.text
-#             print(text)
-        
-#             if "WINNER:" in text:
-#                 winner_start_index = text.find("WINNER:") + len("WINNER:")
-#                 winner_text = text[winner_start_index:].strip()
-#                 winner_but_no_label = text.replace("WINNER: ", "")
-#                 #print(winner_but_no_label)
-
-#         print("Winner text:", winner_text)
-        
-        # to seperate the artist by the song, do whatever is in quotes
-        # is there something that seperates it by italics
-
-
-
-
-
-            
+def get_winners ():
+    #this function gets the winners for every category, and adds it to a list
 
         
     
